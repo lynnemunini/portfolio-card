17b468a3846ab0e9bcdf9e90bd789209df58f103
lynnemunini <muninilynne65@gmail.com>
2022-10-19 18:52:39 +0300

    Add QuickSand FontFamily

--- app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -9,10 +9,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -45,7 +42,7 @@ fun CreateBizCard() {
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()
-    ){
+    ) {
         Card(
             modifier = Modifier
                 .width(200.dp)
@@ -59,10 +56,24 @@ fun CreateBizCard() {
                 modifier = Modifier.height(300.dp),
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
-            ){
+            ) {
                 CreateImageProfile()
                 Divider(color = Color.Gray, thickness = 0.5.dp)
-}
+                Text(
+                    text = "Lynne M.",
+                    color = Color.Gray,
+                    modifier = Modifier.padding(top = 10.dp, bottom = 5.dp),
+                    style = MaterialTheme.typography.h1
+                )
+                Text(
+                    text = "Android Engineer",
+                    style = MaterialTheme.typography.body1
+                )
+                Text(
+                    text = "@LynneMunini",
+                    style = MaterialTheme.typography.body1
+                )
+            }
 
         }
     }
@@ -90,5 +101,6 @@ private fun CreateImageProfile() {
 @Composable
 fun DefaultPreview() {
     JetBiZCardTheme {
-        CreateBizCard()    }
+        CreateBizCard()
+    }
 }
--- app/src/main/java/com/example/jetbizcard/ui/theme/Type.kt
+++ app/src/main/java/com/example/jetbizcard/ui/theme/Type.kt
@@ -2,17 +2,29 @@ package com.example.jetbizcard.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.jetbizcard.R
 
+val QuickSand = FontFamily(
+    Font(R.font.quicksand_regular),
+    Font(R.font.quicksand_bold, FontWeight.Bold)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = QuickSand,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
+    ),
+    h1 = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.Bold,
+        fontSize = 26.sp
     )
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
